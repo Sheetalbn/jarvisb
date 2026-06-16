b8344cdcded903372cf255ede3d35413fde3f068
Sheetalbn <86048627+Sheetalbn@users.noreply.github.com>
2026-06-16 16:51:43 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import fetch from 'node-fetch';
+
 
 const app = express();
 app.use(cors()); // This lets your frontend access the backend securely from any URL
